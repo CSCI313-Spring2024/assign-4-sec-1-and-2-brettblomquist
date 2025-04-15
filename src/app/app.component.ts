c3d75145985887c178fe9657408e71bf53f62696
brettblomquist <brett.blomquist@ndsu.edu>
2025-04-15 08:57:40 -0500

    added contacts service and contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { ContactsService } from './contacts.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'assignment-4';
+  contacts: {firstName:string,lastName:string,phoneNumber:string,email:string}[]=[];
+  contactsService = inject(ContactsService);
+  constructor(){}
+  ngOnInit(): void{
+    this.contacts = this.contactsService.contacts;
+  }
 }
---
+++ src/app/contacts.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactsService {
+  contacts = [
+    {
+      firstName: "John",
+      lastName: "Doe",
+      phoneNumber: "111-111-1111",
+      email: "johndoe@gmail.com"
+    },
+    {
+      firstName: "Jane",
+      lastName: "Roe",
+      phoneNumber: "222-222-2222",
+      email: "janeroe@gmail.com"
+    }
+  ]
+
+  constructor() { }
+
+  addContact(firstName: string, lastName: string, phoneNumber: string, email: string){
+    this.contacts.push({firstName, lastName, phoneNumber, email})
+  }
+}
